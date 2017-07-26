3209a16ee80eb2e58750f2b3df07f4ecb2798fa7
Vedanti Gulalkari <vedantigulalkari@gmail.com>
2017-07-26 15:51:36 +0530

    added initial two files

---
+++ Index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+
+</head>
+<body>
+<form action="Result.php" method="post">
+    Name: <input type="text" name="name">
+
+    E-mail: <input type="text" name="email">
+
+    Gender:
+    <input type="radio" name="gender"
+
+           value="female">Female
+    <input type="radio" name="gender"
+           value="male">Male
+    <input type="submit" name="submit">
+
+</form>
+</body>
+</html>
+
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Vedanti
+ * Date: 26-07-2017
+ * Time: 14:57
+ */
+?>
---
+++ Result.php
@@ -0,0 +1,9 @@
+<?php
+echo "Congratulations!Welcome to JPMC";
+/**
+ * Created by PhpStorm.
+ * User: Vedanti
+ * Date: 26-07-2017
+ * Time: 14:59
+ */
+?>
